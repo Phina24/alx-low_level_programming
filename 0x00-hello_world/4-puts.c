50fa3d6293bc4fcae22fe8eafa1ec96c452c6561
Stephina <stephmeche@gmail.com>
2023-02-20 08:00:11 -0800

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 int main(void)
+/**
+ * prints the string "Programming is li building a multilingual puzzle"
+ */
 {
 	char mystring[] = "Programming is like building a multilingual puzzle";
 	puts(mystring);
