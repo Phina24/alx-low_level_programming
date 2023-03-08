4d619f3d3f157ac3860f682520f665f00dfbb1bc
Stephina <stephmeche@gmail.com>
2023-03-07 16:33:28 -0800

    A function that locates a substring.

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - finds the 1st occurrence of a substring in a string
+ * @haystack: string
+ * @needle: substring
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+
+		if (*b == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
