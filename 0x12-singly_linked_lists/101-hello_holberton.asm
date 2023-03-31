ff0f20d64b0c78b6665c77c9c3dd1a9c59ecccdd
Stephina <stephmeche@gmail.com>
2023-03-30 19:08:50 -0700

    C-singly linked list task and solutions

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+extern printf ;used to print a string folowed by a new line
+
+global main ;a 64-bit program in assembly
+
+section .text
+main:
+	push msg
+	call printf
+	ret
+
+section .data
+msg: db 'Hello, Holberton',10, 0
